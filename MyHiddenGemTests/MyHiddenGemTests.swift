1ac882068f8375238773739a9a8974131686776c
89Explorer <leral3@naver.com>
2025-04-23 05:25:24 +0900

    Initial Commit

---
+++ MyHiddenGemTests/MyHiddenGemTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyHiddenGemTests.swift
+//  MyHiddenGemTests
+//
+//  Created by 권정근 on 4/23/25.
+//
+
+import Testing
+@testable import MyHiddenGem
+
+struct MyHiddenGemTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
